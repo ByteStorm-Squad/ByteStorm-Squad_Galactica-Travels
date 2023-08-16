0a0f73f84385959683abab4de08261f706cab4cf
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-16 21:49:28 +0530

    Text box is created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavMenu, Hero, Footer, Memory, Explore, Advertise, Pricings, Banner, Newslatter, Background } from './components';
 import { hero, navlinks, memory, placesAPI, brands, pricingapi, bannerAPI, footerAPI } from './data/travigodata';
+import TextBox from '../src/components/TextBox/TextBox';
 
 const App = () => {
   return (
     <>
       <Background />
+      <TextBox text="First Name*"/>
       {/* <NavMenu navlinks={navlinks} />
       <Memory memory={memory} />
       <Explore title="Explore The Beauty of World" placesAPI={placesAPI} />
