c52b2a4cf9040af61906dc709b4c0fa5ad26c81a
Anoshan Jayahanthan <anoshan265@gmail.com>
2023-08-16 21:22:30 +0530

    update login routes

--- backend/src/routes/login.routes.js
+++ backend/src/routes/login.routes.js
@@ -21,55 +21,10 @@ router.post("/login", function(req, res){
     registrationController.getUserByEmail(req, res);
 });
 
-router.get("/staff_login", function(req, res){
-  res.render('staff_login', {docTitle: "STAFF LOGIN", flash: res.locals.flash , userRole: req.cookies.userRole });
-
-});
-
-router.post("/staff_login", function(req, res){
-    registrationController.getStaffByEmail(req, res);
-});
-
 router.get("/logout", function(req, res){
     res.clearCookie('userRole');
     res.redirect('/');
 });
-
-// router.get('/admin', checkRole('Admin'), (req, res) => {
-//   // The user is an admin, so allow them to access the route
-//   res.send('Welcome to the admin page!');
-// });
-
-// router.get('/managerdashboard',checkRole('Manager'), (req, res) => {
-//   // The user is a manager, so allow them to access the route
-  
-//   try{
-//     const formData = req.body;
-//     registrationController.getStaffByEmail(formData,res);
-//   }catch (err) {
-//     console.log(err);
-//     res.send("1500");
-// }
-// });
-
-/*
-router.get('/managerDashboard', checkRole('Manager'), (req, res) => {
-  // The user is a manager, so allow them to access the route
-  res.send('Welcome to the manager page!');
-});
-*/
-// router.get('/userDashboard', checkRole('user'), (req, res) => {
-//   // The user is a user, so allow them to access the route
-//   try {
-//           //res.redirect('/userDashboard');
-//           const formData = req.body;
-//           registrationController.getUserDetails(formData, res);
-      
-//         } catch (err) {
-//             console.log(err);
-//             res.send("1500");
-//         }
-// });
 
 
 module.exports = router;
