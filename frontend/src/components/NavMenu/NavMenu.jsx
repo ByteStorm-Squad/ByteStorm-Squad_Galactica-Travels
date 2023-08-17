bbb84c26b5ef59ceb0641e8f91c8102a8328ca5b
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2023-08-17 19:53:52 +0530

    Create circular menu

--- frontend/src/components/NavMenu/NavMenu.jsx
+++ frontend/src/components/NavMenu/NavMenu.jsx
@@ -1,74 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import logo from '../../images/logo.png';
-import menu from '../../images/menu.svg';
-import PopupMenu from '../PopupMenu';
+import React, { useState } from 'react';
+import { Home, Person, SpaceDashboard, RocketLaunch, Map, WbSunny } from '@mui/icons-material';
+import './styles.css';
 
-const NavMenu = ({ navlinks }) => {
-  const [popupState, setPopupState] = useState(false);
-  const [navState, setNavState] = useState(false);
-  const onTriggerPopup = () => setPopupState(!popupState);
-
-  const onNavScroll = () => {
-    if (window.scrollY > 180) {
-      setNavState(true);
-    } else {
-      setNavState(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', onNavScroll);
-
-    return () => {
-      window.removeEventListener('scroll', onNavScroll);
-    };
-  }, []);
-
-  // console.log(popupState)
+const NavMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <>
-      <header
-        className={`
-        nav-default ${navState && 'nav-sticky'}
-      `}
-      >
-        <nav className="flex items-center justify-between travigo-container">
-          <NavLink to={`/`} className="flex items-center">
-            <img src={logo} alt="logo/img" className="w-22 h-9 object-fill" />
-          </NavLink>
-          <ul className="flex items-center lg:hidden gap-7">
-            {navlinks?.map((val, i) => (
-              <li key={i}>
-                <NavLink to={'#'} className="text-lg text-slate-900">
-                  {val.link}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-          <ul className="flex items-center lg:hidden">
-            <li>
-              <button type="button" className="button-emrald px-7 text-base">
-                Join Us
-              </button>
-            </li>
-          </ul>
-          <ul className="hidden lg:flex items-center">
-            <li>
-              <button
-                type="button"
-                className="flex items-center justify-center transition-all duration-200 active:scale-90 cursor-pointer"
-                onClick={onTriggerPopup}
-              >
-                <img src={menu} alt="menu/svg" className="object-cover shadow-sm filter" />
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <PopupMenu navlinks={navlinks} popupState={popupState} />
-    </>
+    <nav className={isMenuOpen ? 'open' : 'close'}>
+      <div className="nav-content">
+        <div className="toggle-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <WbSunny />
+        </div>
+        <span style={{ '--i': 1 }}>
+          <a href="#">
+            <Map />
+          </a>
+        </span>
+        <span style={{ '--i': 2 }}>
+          <a href="#">
+            <RocketLaunch />
+          </a>
+        </span>
+        <span style={{ '--i': 3 }}>
+          <a href="#">
+            <SpaceDashboard />
+          </a>
+        </span>
+        <span style={{ '--i': 4 }}>
+          <a href="#">
+            <Person />
+          </a>
+        </span>
+        <span style={{ '--i': 5 }}>
+          <a href="#">
+            <Home />
+          </a>
+        </span>
+      </div>
+    </nav>
   );
 };
 
