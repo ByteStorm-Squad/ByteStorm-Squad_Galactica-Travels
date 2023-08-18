daecd199c43b79fab8066ef734c0fd1cc47ed5bc
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2023-08-19 01:51:58 +0530

    Cleanup Signup screen

--- frontend/src/screens/SignupPage/SignupPage.jsx
+++ frontend/src/screens/SignupPage/SignupPage.jsx
@@ -25,96 +25,31 @@ export const SignupPage = () => {
   );
   const dateValue = new Date(new Date().setDate(14));
   return (
-    <div className="signup-page">
-      <div className="overlap-group-wrapper">
-        <div className="overlap-group">
-          <div className="text-wrapper">SignUp</div>
-          <div className="div">SignUp Details</div>
-          <div className="component">
-            <TextBox className="input-text-here" text="First Name*" type="text" />
-            <div className="text-field-details">0/20</div>
-          </div>
-          <div className="component-2">
-            <TextBox className="input-text-here" text="Contact Identifier" type="text" />
-            <div className="text-field-details">0/20</div>
-          </div>
-          <div className="component-3">
-            <TextBox className="input-text-here" text="Last Name*" type="text" />
-            <div className="text-field-details">0/20</div>
-          </div>
-          <div className="overlap">
-           
-              <Box sx={{ minWidth: 240 }}>
-                {' '}
-                {/* Doubling the width by adjusting minWidth */}
-                <LocalizationProvider dateAdapter={AdapterDayjs} className="input-text-here">
-                  <DemoContainer components={['DatePicker']}>
-                    <DatePicker
-                      label="Date of Birth*"
-                      sx={{
-                        '& .MuiInputBase-input': {
-                          color: 'white',
-                        },
-                        '& .MuiInputBase-root': {
-                          backgroundColor: '#333',
-                        },
-                        '& .MuiInputLabel-root': {
-                          color: 'white',
-                        },
-                        width: '200%', // Doubling the width
-                        // Add more custom styles as needed
-                      }}
-                    />
-                  </DemoContainer>
-                </LocalizationProvider>
-              </Box>
-              <div className="text-field-details">0/20</div>
-            {/* <CalendarMonth className="calendar-month" /> */}
-          </div>
-          <div className="component-5">
-            <TextBox className="input-text-here" text="Inter Galactic ID" type="text" />
-            <div className="text-field-details">0/20</div>
-          </div>
-          <div className="overlap-2">
-            <div className="component-6">
-              <Box sx={{ minWidth: 120 }}>
-                <FormControl fullWidth>
-                  <InputLabel
-                    id="demo-simple-select-label"
-                    sx={{ color: 'rgb(181, 164, 164)' }} // Set the input label color to white
-                  >
-                    Home Location
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Age"
-                    sx={{ color: 'rgb(181, 164, 164)' }} // Set the font color to white
-                  >
-                    <MenuItem value={10}>Location 1</MenuItem>
-                    <MenuItem value={20}>Location 2</MenuItem>
-                    <MenuItem value={30}>Location 3</MenuItem>
-                  </Select>
-                </FormControl>
-              </Box>
-              <div className="text-field-details">0/20</div>
-            </div>
-            <img
-              className="arrow"
-              alt="Arrow"
-              src="https://generation-sessions.s3.amazonaws.com/3bcb445300c22561c34e152b4a4056d5/img/arrow-3.svg"
+    <>
+      <Box sx={{ minWidth: 240 }}>
+        {/* Doubling the width by adjusting minWidth */}
+        <LocalizationProvider dateAdapter={AdapterDayjs} className="input-text-here">
+          <DemoContainer components={['DatePicker']}>
+            <DatePicker
+              label="Date of Birth*"
+              sx={{
+                '& .MuiInputBase-input': {
+                  color: 'white',
+                },
+                '& .MuiInputBase-root': {
+                  backgroundColor: '#333',
+                },
+                '& .MuiInputLabel-root': {
+                  color: 'white',
+                },
+                width: '200%', // Doubling the width
+                // Add more custom styles as needed
+              }}
             />
-          </div>
-          <Button className="button-instance" divClassName="design-component-instance-node" text="Submit" type="full" />
-
-          <img
-            className="chevron-left"
-            alt="Chevron left"
-            src="https://generation-sessions.s3.amazonaws.com/3bcb445300c22561c34e152b4a4056d5/img/chevron-left.svg"
-          />
-        </div>
-      </div>
-    </div>
+          </DemoContainer>
+        </LocalizationProvider>
+      </Box>
+    </>
   );
 };
 
