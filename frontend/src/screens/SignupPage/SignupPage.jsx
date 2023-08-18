331aee544f96a2c6778e5c4f3a587ab5abf00331
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2023-08-19 01:53:44 +0530

    Remove unncessary imports in SignUp page

--- frontend/src/screens/SignupPage/SignupPage.jsx
+++ frontend/src/screens/SignupPage/SignupPage.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { Button } from '../../components/Button/Button';
 import { CalendarMonth } from '../../icons/CalendarMonth/CalendarMonth';
-import TextBox from '../../components/TextBox/TextBox';
-import './SignupPage.css';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
 export const SignupPage = () => {
-  const minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 7, 0, 0, 0);
-  const maxDate = new Date(
-    new Date().getFullYear(),
-    new Date().getMonth(),
-    27,
-    new Date().getHours(),
-    new Date().getMinutes(),
-    new Date().getSeconds(),
-  );
-  const dateValue = new Date(new Date().setDate(14));
   return (
     <>
       <Box sx={{ minWidth: 240 }}>
