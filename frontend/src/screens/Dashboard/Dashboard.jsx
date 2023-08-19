2ee1c65eefae5cc8ffd8e64a07251f08789db3bc
Anoshan Jayahanthan <anoshan265@gmail.com>
2023-08-19 17:20:02 +0530

    update dashboard jsx

--- frontend/src/screens/Dashboard/Dashboard.jsx
+++ frontend/src/screens/Dashboard/Dashboard.jsx
@@ -1,7 +1,103 @@
 import React from 'react';
 
-const Dashboard = () => {
-  return <div>This is the Dashboard page</div>;
+import "./style.css";
+
+export const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <div className="overlap-wrapper">
+                <div className="overlap">
+                    <img className="paul-volkmer" alt="Paul volkmer" src="paul-volkmer-qvotvbsum-c-unsplash-1.png" />
+                    <div className="text-wrapper">Dashboard</div>
+                    <div className="frame">
+                        <div className="group">
+                            <div className="overlap-group">
+                                <div className="popular-now">Upcoming</div>
+                            </div>
+                        </div>
+                        <div className="group">
+                            <div className="overlap-group">
+                                <div className="popular-now">Popular</div>
+                            </div>
+                        </div>
+                        <div className="overlap-group-wrapper">
+                            <div className="overlap-group">
+                                <div className="popular-now">Highly Rated</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div">
+                        <div className="image-wrapper">
+                            <img className="image" alt="Image" src="image-2.png" />
+                        </div>
+                    </div>
+                    <div className="frame-2">
+                        <div className="group-2">
+                            <div className="div-wrapper">
+                                <div className="text-wrapper-2">Events</div>
+                            </div>
+                        </div>
+                        <div className="group-2">
+                            <div className="div-wrapper">
+                                <div className="popular-destinations">Attractions</div>
+                            </div>
+                        </div>
+                        <div className="group-3">
+                            <div className="div-wrapper">
+                                <div className="unique-cultures">Cultures</div>
+                            </div>
+                        </div>
+                    </div>
+                    <img className="union" alt="Union" src="union.svg" />
+                    <img className="rectangle" alt="Rectangle" src="rectangle-4321.svg" />
+                    <div className="text-wrapper-3">Book Now</div>
+                    <div className="flexcontainer">
+                        <p className="text">
+                            <span className="span">
+                                Distance&nbsp;&nbsp;: 1.87 Light Years
+                                <br />
+                            </span>
+                        </p>
+                        <p className="text">
+                            <span className="text-wrapper-4">
+                                Travel Modes starting from
+                                <br />
+                            </span>
+                        </p>
+                        <p className="text">
+                            <span className="text-wrapper-5">$16000</span>
+                            <span className="text-wrapper-6">&nbsp;</span>
+                        </p>
+                    </div>
+                    <div className="text-wrapper-7">Zephyria</div>
+                    <div className="group-4">
+                        <div className="overlap-2">
+                            <div className="rectangle-wrapper">
+                                <div className="rectangle-2" />
+                            </div>
+                            <div className="group-5">
+                                <div className="overlap-group-2">
+                                    <div className="rectangle-3" />
+                                    <img className="img" alt="Rectangle" src="rectangle-15.png" />
+                                    <div className="rectangle-4" />
+                                </div>
+                            </div>
+                            <div className="img-wrapper">
+                                <img className="rectangle-5" alt="Rectangle" src="rectangle-7.png" />
+                            </div>
+                            <img className="group-6" alt="Group" src="group-7.png" />
+                        </div>
+                        <p className="explore-zephyria-s">
+                            Explore Zephyria&#39;s Enchanted Arboretum: A mesmerizing fusion of flora and fauna, painting an
+                            intergalactic masterpiece.
+                        </p>
+                    </div>
+                    <div className="frame-3">
+                        <div className="rectangle-6" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
-export default Dashboard;
