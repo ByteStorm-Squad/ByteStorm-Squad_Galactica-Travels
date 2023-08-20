f662653430ae610ca27f666423451fdea7b56a67
Amila29 <amilakasun2000@gmail.com>
2023-08-20 20:09:20 +0530

    loginpage created

--- frontend/src/screens/Dashboard/Dashboard.jsx
+++ frontend/src/screens/Dashboard/Dashboard.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import LoginPage from '../LoginPage/loginPage';
 
 
 
 const Dashboard = () => {
  
   return (
-  <div>
-    This is the Dashboard page
-    
-    </div>);
+    <div>
+      This is the Dashboard page
+      
+    </div>
+  );
   
 };
 
